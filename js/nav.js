f8c9ac1fab6893d2bb6ae337c73795cfc90fda0a
Terry Li <terryseanli@gmail.com>
2023-03-02 15:03:21 -0800

    added new story form, submit link, and function to show new story form

---
+++ js/nav.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $mainNavLinks.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+/** Shows submit form on click on "Submit" */
+
+function navSubmitClick(evt) {
+  console.debug("navSubmitClick", evt);
+  evt.preventDefault();
+  $submitForm.show();
+}
+
+$navSubmit.on("click", navSubmitClick);
